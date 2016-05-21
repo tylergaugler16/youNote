ccbb99dc5b05f782c645a76d1bc28b28957e2fab
Tyler Gaugler <tylergaugler16@gmail.com>
2016-05-21 16:09:45 -0400

    added public attribute to notes to make selectable notes seen by other users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512214850) do
+ActiveRecord::Schema.define(version: 20160521195626) do
 
   create_table "notes", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160512214850) do
     t.datetime "updated_at",      null: false
     t.string   "content_item_id"
     t.integer  "user_id"
+    t.boolean  "public"
   end
 
   create_table "users", force: :cascade do |t|
